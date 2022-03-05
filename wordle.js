1f89637938137a63a8c87146e1207f0c48eaddec
unknown <duy822@ymail.com>
2022-03-05 17:52:16 +0700

    Bugs fixed

--- wordle.js
+++ wordle.js
@@ -1,8 +1,5 @@
-let key = "apply";
-let guess = "happy";
-
-console.log(key);
-console.log(guess);
+let key = "bóhoa";
+let guess = "conbò";
 
 let vowels = [
     [ 'a', 'á', 'à', 'ã', 'ả', 'ạ' ],
@@ -31,9 +28,31 @@ const vowelCheck = (a, b) => {
     return false;
 }
 
+const strToObj = (word) => {
+    let obj = {};
+    for(let i = 0; i < word.length; i++) {
+        if(!(word[i] in obj)) {
+            obj[word[i]] = 1;
+        } else {
+            obj[word[i]]++;
+        }
+    }
+
+    return obj;
+}
+
 const wordle = (key, guess) => {
+    console.log(key);
+    console.log(guess);
+
     let result = new Array(key.length); 
     for(let i = 0; i < key.length; i++) result[i] = 0;
+
+    let keyObj = strToObj(key);
+    let guessObj = strToObj(guess);
+    
+    console.log(keyObj);
+    console.log(guessObj);
 
     for(i = 0; i < key.length; i++) {
         let keyChar = key.charAt(i);
@@ -41,33 +60,62 @@ const wordle = (key, guess) => {
 
         if(keyChar == guessChar) {
             result[i] = 4;
+
+            keyObj[keyChar]--;
+            guessObj[guessChar]--;
+
+            console.log("Match: " + keyChar);
+            console.log(keyObj);
+            console.log(guessObj);
         } else if(vowelCheck(keyChar, guessChar)) {
             result[i] = 3;
-        }
+
+            keyObj[keyChar] -= 1;
+            guessObj[guessChar] -= 1;
+
+            console.log("Almost match: " + guessChar);
+            console.log(keyObj);
+            console.log(guessObj);
+        } 
     }
 
-    //console.log(result);
+    //console.log(result); 
 
     for(i = 0; i < key.length; i++) {
         if(result[i] < 3) {
-            let j = -1;
-            while ((j = key.indexOf(guess.charAt(i), j+1)) != -1){
-                //console.log(key.charAt(j), result[j], j, i);
+            let keyChar = key.charAt(i);
+            let guessChar = guess.charAt(i);
 
-                if(result[j] < 3) {
-                    result[i] = 2;
-                    //console.log(result);
-                    break;
-                }
+            if((guessChar in keyObj) && (keyObj[guessChar] > 0)) {
+                result[i] = 2;
+
+                keyObj[guessChar] -= 1;
+                guessObj[guessChar] -= 1;
+                
+                console.log("Miss: " + guessChar);
+                console.log(keyObj);
+                console.log(guessObj);
             }
         }
     }
 
     for(i = 0; i < key.length; i++) {
         if(result[i] < 2) {
+            let guessChar = guess.charAt(i);
+            
             for(j = 0; j < key.length; j++) {
-                if((result[j] == 0) && (vowelCheck(guess.charAt(i), key.charAt(j)))) {
+                let keyChar = key.charAt(j);
+
+                if((vowelCheck(guessChar, keyChar)) && (keyObj[keyChar] > 0)) {
                     result[i] = 1;
+
+                    keyObj[guessChar] -= 1;
+                    guessObj[guessChar] -= 1;
+                
+                    console.log("Almost miss: " + guessChar);
+                    console.log(keyObj);
+                    console.log(guessObj);
+
                     break;
                 }
             }
